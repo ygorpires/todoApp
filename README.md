e7774d88d24cbf67622f900cc9a481b243e3812a
ygorpires <ygor_pires@hotmail.com>
2022-01-10 22:40:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# todoApp
+
+
+App de controle de tarefas desenvolvido em React.js no front-end e Node.js e banco de dados Mongodb no backend com o intuído aprendizado das tecnologias citadas acima.
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/IconButton'
+
+export default props =>{
+
+    const keyHandler = (e) =>{
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }else if(e.key === 'Escape'){
+            props.handleClear()
+        }
+    }
+
+    return(
+        <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='desciption' className='form-control' placeholder='Adicine uma tarefa'
+                onChange={props.handleChange}
+                onKeyUp={keyHandler}
+                value={props.description}></input>
+        </Grid>
+
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
+
+            <IconButton style='info' icon='search'
+                onClick={props.handleSearch}></IconButton>  
+
+            <IconButton style='default' icon='close'
+                onClick={props.handleClear}></IconButton>      
+         
+        </Grid>
+               
+    </div>
+    )
+    
+    
+}
